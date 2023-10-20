29e2ce89ee69dce1dcb84c93d86311b5d0ce49f8
Vyacheslav <torbin.official@gmail.com>
2023-10-20 19:51:30 +0300

    Annotations changed from NotNull to NotBlank, Min(0) to PositiveOrZero

--- src/main/java/org/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/bookstore/dto/CreateBookRequestDto.java
@@ -1,21 +1,20 @@
 package org.bookstore.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     private Long id;
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String isbn;
-    @NotNull
-    @Min(0)
+    @PositiveOrZero
     private BigDecimal price;
     private String description;
     private String coverImage;
