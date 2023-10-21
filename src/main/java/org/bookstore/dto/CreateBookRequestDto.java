553a5b12035e9fbb94d63d1701fc1683aeef39d6
Vyacheslav <torbin.official@gmail.com>
2023-10-22 00:30:56 +0300

    Added dependency, CreateBookRequestDto converted to record

--- src/main/java/org/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/bookstore/dto/CreateBookRequestDto.java
@@ -1,21 +1,21 @@
 package org.bookstore.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
-import lombok.Data;
 
-@Data
-public class CreateBookRequestDto {
-    private Long id;
+public record CreateBookRequestDto(
+    Long id,
+    @NotBlank String title,
     @NotBlank
-    private String title;
+    String author,
     @NotBlank
-    private String author;
-    @NotBlank
-    private String isbn;
+    String isbn,
+    @NotNull
     @PositiveOrZero
-    private BigDecimal price;
-    private String description;
-    private String coverImage;
-}
+    BigDecimal price,
+    String description,
+    String coverImage
+)
+{}
