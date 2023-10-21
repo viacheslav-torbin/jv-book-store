e3a7d2dbb2b92806099eb22bb5ce9163a723034b
Vyacheslav <torbin.official@gmail.com>
2023-10-22 00:32:51 +0300

    Checkstyle error fixed

--- src/main/java/org/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/bookstore/dto/CreateBookRequestDto.java
@@ -6,16 +6,16 @@ import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 
 public record CreateBookRequestDto(
-    Long id,
-    @NotBlank String title,
-    @NotBlank
-    String author,
-    @NotBlank
-    String isbn,
-    @NotNull
-    @PositiveOrZero
-    BigDecimal price,
-    String description,
-    String coverImage
+        Long id,
+        @NotBlank String title,
+        @NotBlank
+        String author,
+        @NotBlank
+        String isbn,
+        @NotNull
+        @PositiveOrZero
+        BigDecimal price,
+        String description,
+        String coverImage
 )
 {}
