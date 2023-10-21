cc82c3d48918fd7f7091c948e86bb73f940c88f8
Vyacheslav <torbin.official@gmail.com>
2023-10-22 00:50:32 +0300

    Custom exception handling added Validated annotation for handlers class

--- src/main/java/org/bookstore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/bookstore/exceptions/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -26,13 +27,28 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("time", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST);
         List<String> error = ex.getBindingResult().getAllErrors()
                 .stream()
                 .map(this::getErrorMessage)
                 .collect(Collectors.toList());
         body.put("errors", error);
         return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    private ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("errors", ex.getMessage());
+        HttpStatusCode status = HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(body, status);
+    }
+
+    @ExceptionHandler(value = {DataProcessingException.class})
+    private ResponseEntity<Object> handleDataProcessingException(EntityNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("errors", ex.getMessage());
+        HttpStatusCode status = HttpStatus.I_AM_A_TEAPOT;
+        return new ResponseEntity<>(body, status);
     }
 
     private String getErrorMessage(ObjectError objectError) {
