b3bcdc834198d9e023b83e4666d1f159fbcc48d8
Vyacheslav <torbin.official@gmail.com>
2023-10-23 00:18:29 +0300

    Pagination, Sorting and Swagger added Also added maven dependency

--- src/main/java/org/bookstore/controller/BookController.java
+++ src/main/java/org/bookstore/controller/BookController.java
@@ -1,5 +1,7 @@
 package org.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
@@ -7,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.bookstore.dto.BookDto;
 import org.bookstore.dto.CreateBookRequestDto;
 import org.bookstore.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Books management",description = "Endpoints for book management")
 @Validated
 @RequiredArgsConstructor
 @RestController
@@ -24,27 +28,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Getting all books")
     @GetMapping
-    List<BookDto> getAll() {
-        return bookService.findAll();
+    List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Getting book by id")
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable @Positive Long id) {
         return bookService.findById(id);
     }
 
+    @Operation(summary = "Creating book")
     @PostMapping
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookRequestDto) {
         return bookService.save(bookRequestDto);
     }
 
+    @Operation(summary = "Updating book by id")
     @PutMapping("/{id}")
     public void updateBookById(@PathVariable @Positive Long id,
                                @RequestBody @Valid CreateBookRequestDto bookRequestDto) {
         bookService.updateBookById(id, bookRequestDto);
     }
 
+    @Operation(summary = "Deleting book by id")
     @DeleteMapping("/{id}")
     public void deleteBookById(@PathVariable @Positive Long id) {
         bookService.deleteById(id);
--- src/main/java/org/bookstore/service/BookService.java
+++ src/main/java/org/bookstore/service/BookService.java
@@ -3,11 +3,12 @@ package org.bookstore.service;
 import java.util.List;
 import org.bookstore.dto.BookDto;
 import org.bookstore.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto bookRequestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
